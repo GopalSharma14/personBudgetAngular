493e267290e4b0acf1f22e53ddb35102c4c6e18c
GopalSharma14 <cse.gopalsharma@gmail.com>
2020-10-07 21:11:33 -0400

    1st commit

---
+++ personal-budget/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArticleComponent } from './article/article.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HeroComponent,
+    FooterComponent,
+    HomepageComponent,
+    ArticleComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
